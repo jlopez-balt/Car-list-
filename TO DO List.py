1e2648606dacc5528425357616c859b52429d6d9
jlopez-balt <157404957+jlopez-balt@users.noreply.github.com>
2024-01-22 11:36:19 -0600

    Add files via upload
    uploading python script for to-do list

---
+++ TO DO List.py
@@ -0,0 +1,34 @@
+#Jaime Lopez and Luis Jacinto 
+# 1/11/23 
+# To-do List 
+def bold_text(text):
+    bold_start = '\033[1m'
+    bold_end = '\033[0m'
+    return bold_start + text + bold_end
+
+def complete (word):
+    word=input("what would u like to mark as done")
+    bold_text(word.upper())
+
+list=["hellcat", "Bugaati","bentley", "benz", "Genisis", "Amg GT","trackhawk", "M5", "E30", "SS"]
+while True: 
+    choice = input(print("What would you like to do? \n1. add \n2. view \n3. mark done \n4. remove \n5. exit \n6. clear \n7. sort \n8. tasks"))
+    if choice == "add":
+        list.append(input("what would you like to add"))
+    if choice == "view":
+        print(list)
+    if choice == "done":
+        word=input("what would u like to mark as done")
+        x=list.index(word)
+        list[x]=(word +" x")
+        print(list)
+    if choice == "remove":
+        list.remove(input("What would lou like to remove"))
+    if choice == "exit":
+        quit()
+    if choice == "clear": 
+        list.clear()
+    if choice == "sort":
+        list.sort()
+    if choice == "tasks":
+        print(len(list))
